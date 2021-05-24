3c1692e7e93c66e99e3c241cfcfb5a85a15e2a78
LunaticHacker <lunatichacker@tutanota.com>
2021-05-24 20:56:50 +0530

    the trending feed works now

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var app = new Vue({
   data: {
     videos: [],
     state: "Loading...",
-    avatar: "https://placekitten.com/100/100",
     name: "",
     title: "Bringing Back TikTok",
     signature: "Find videos by Username",
@@ -13,9 +12,6 @@ var app = new Vue({
       try {
         this.state = "Loading...";
         this.videos = [];
-        this.avatar = "https:placekitten.com/100/100";
-        this.title = "Bringing Back TikTok";
-        this.signature = "Find videos by Username";
         res = await fetch(
           "https://tiktokrestapi.herokuapp.com/trending"
         );
@@ -23,26 +19,6 @@ var app = new Vue({
 
         this.videos = json.collector;
         this.state = null;
-      } catch (error) {
-        this.videos = [];
-        this.state = null;
-      }
-    },
-    getUser: async function () {
-      try {
-        this.state = "Loading...";
-        this.videos = [];
-        res = await fetch(
-          `https://cors-anywhere.herokuapp.com/https://tiktokrestapi.herokuapp.com/user/${this.name}`
-        );
-        json = await res.json();
-        this.videos = json.collector;
-        this.state = null;
-        if (this.videos.length > 0) {
-          this.avatar = this.videos[0].authorMeta.avatar;
-          this.title = this.videos[0].authorMeta.nickName;
-          this.signature = this.videos[0].authorMeta.signature;
-        }
       } catch (error) {
         this.videos = [];
         this.state = null;
